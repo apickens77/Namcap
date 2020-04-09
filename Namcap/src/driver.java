2f8aec19d41aca930283c85b25fd01dce2cb44b3
Adam Pickens <Adam Pickens@MSI.cinci.rr.com>
2020-04-08 22:27:35 -0400

    Added basic controllable move function

--- Namcap/src/driver.java
+++ Namcap/src/driver.java
@@ -1,13 +1,21 @@
+import javax.swing.JFrame;
 
 public class driver {
 
 	public static void main(String[] args) {
-		Graph b=new Graph();
+		/*Graph b=new Graph();
 		b.print();
 		b.addEdge(3, 1);
 		b.print();
 		//b.removeEdge(3, 1);
-		b.print();
+		b.print();*/
+		
+		JFrame f = new JFrame();
+		Move m = new Move();
+		f.add(m);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(800, 600);
 	}
 
 }
