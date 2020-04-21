759faaf70f7e4021223085b1a866fae53530decf
Adam Pickens <Adam Pickens@192.168.1.107>
2020-04-20 23:17:13 -0400

    ghost will show up and move right, but program crashes for some reason

--- Namcap/src/Move.java
+++ Namcap/src/Move.java
@@ -1,12 +1,17 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Move extends JPanel implements ActionListener, KeyListener
 {
 	Timer t = new Timer(5, this);
-	double x = 0, y = 0, velx = 0, vely;
+	int x = 0, y = 0, velx = 0, vely;
+	BufferedImage image;
 	
 	public Move()
 	{
@@ -15,13 +20,32 @@ public class Move extends JPanel implements ActionListener, KeyListener
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
 		
+		BufferedImage img = null;
+		JFileChooser fc = new JFileChooser();
+		//fc.setCurrentDirectory(new);
+		int result = fc.showOpenDialog(this);
+		File file = fc.getSelectedFile();
+		
+		try {
+			img = ImageIO.read(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("FUCK");
+		}	
+		System.out.println("Selected file: " + file.getAbsolutePath());
+		image = img;
+		
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		Graphics2D g2 = (Graphics2D) g;
-		g2.fill(new Ellipse2D.Double(x, y, 40, 40));
+		Graphics2D g2 = (Graphics2D) g.create();
+		
+		g2.drawImage(image, x, y, this);
+		
+		
+		//g2.fill(new Ellipse2D.Double(x, y, 40, 40));
 	}
 	
 	public void actionPerformed(ActionEvent e)
@@ -33,25 +57,32 @@ public class Move extends JPanel implements ActionListener, KeyListener
 	
 	public void up()
 	{
-		vely = -1.5;
+		vely = -1;
 		velx = 0;
+		//t.wait(250);
 	}
 	
 	public void down()
 	{
-		vely = 1.5;
+		vely = 1;
 		velx = 0;
 	}
 	
 	public void left()
 	{
-		velx = -1.5;
+		velx = -1;
 		vely = 0;
 	}
 	
 	public void right()
 	{
-		velx = 1.5;
+		velx = 1;
+		vely = 0;
+	}
+	
+	public void stop()
+	{
+		velx = 0;
 		vely = 0;
 	}
 	
@@ -62,10 +93,14 @@ public class Move extends JPanel implements ActionListener, KeyListener
 		if (code == KeyEvent.VK_UP)
 		{
 			up();
+			
 		}
 		if (code == KeyEvent.VK_DOWN)
 		{
-			down();
+			double start = y;
+			while (start+50 != y)
+				down();
+			stop();
 		}
 		if (code == KeyEvent.VK_LEFT)
 		{
@@ -78,5 +113,9 @@ public class Move extends JPanel implements ActionListener, KeyListener
 	}
 	
 	public void keyTyped(KeyEvent e) {}
-	public void keyReleased(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) 
+	{
+		//stop();
+		//System.out.println("x: " + x + " y: " + y);
+	}
 }
--- Namcap/src/driver.java
+++ Namcap/src/driver.java
@@ -1,7 +1,19 @@
+import java.awt.*;
 
-public class driver {
+import javax.imageio.ImageIO;
+//import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.border.TitledBorder;
+
+public class driver extends JPanel {
 	public static void main(String[] args)
 	{
+		/*
 		Board board = new Board(7, 5);
 		System.out.println(board);
 		System.out.println();
@@ -11,8 +23,28 @@ public class driver {
 		System.out.println();
 		System.out.println();
 		System.out.println();
-		board.pacman.locateNearestDot();
+		board.pacman.locateNearestDot();*/
 
+		/*BufferedImage image = null;
+		//Image img = getToolkit().getImage("node.jpg");
+
+		
+		try {
+			image = ImageIO.read(new File("ghost.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+		JFrame f = new JFrame();
+		Move m = new Move();
+		f.add(m);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(800, 600);
+		
+		//m.x = 10;
+		//m.moveVert(-20);
 		
 	}
 }
